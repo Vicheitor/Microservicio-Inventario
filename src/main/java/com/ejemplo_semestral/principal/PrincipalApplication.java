bd808846eacb206575623f37d82e12c53553b2b2
Vicente Wiehoff <vi.wiehoff@duocuc.cl>
2025-05-22 21:12:00 -0400

    Funciona el .env

--- src/main/java/com/ejemplo_semestral/principal/PrincipalApplication.java
+++ src/main/java/com/ejemplo_semestral/principal/PrincipalApplication.java
@@ -3,11 +3,17 @@ package com.ejemplo_semestral.principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class PrincipalApplication {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PrincipalApplication.class, args);
-	}
-
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+        SpringApplication.run(PrincipalApplication.class, args);
+    }
 }
+
